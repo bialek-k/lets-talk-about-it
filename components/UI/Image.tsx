ce4f1e384ca35f204d6fff86fd3956b30b5dbb44
bialek-k <bialekdev@gmail.com>
2024-05-07 21:33:34 +0200

    fix

--- components/UI/Image.tsx
+++ components/UI/Image.tsx
@@ -11,7 +11,7 @@ export const Image = ({ image }: ImageProps) => {
   return (
     <div className="relative w-64 h-64 container mx-auto my-12">
       <div className="absolute inset-6 bg-[#E2FF00] -left-6 -bottom-6" />
-      <div className="w-full bg-red-400">
+      <div className="w-full bg-red-500">
         {image && <DatoImage data={image} className="" layout="fill" />}
       </div>
     </div>
