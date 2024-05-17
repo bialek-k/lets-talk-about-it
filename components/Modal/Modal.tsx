f4128922cea363b8cf45e36b75d0e26140ea17bb
Okazaki92 <vincentslominski@gmail.com>
2024-05-17 21:27:08 +0200

    feat/add modal component

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,35 +1,11 @@
-'use client';
-
-import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import ChevronIcon from '@/IconsSVG/ChevronIcon';
-import { fetchEvents } from '@/routes/routes';
-import Linkedin from '../Linkedin/Linkedin';
-import Facebook from '../Facebook/Facebook';
-import Youtube from '../Youtube/Youtube';
-import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import { AnimatePresence, motion } from 'framer-motion';
-import MainLogo from '../MainLogo/MainLogo';
-import toRoman from '../UI/NumberToRoman';
 
-interface MobileNavProps {
+interface ModalProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
-  location: string;
+  data?: string | null;
 }
-
-export default function MobileNav({
-  isOpen,
-  setIsOpen,
-  location,
-}: MobileNavProps) {
-  const [isSubMenuOpen, setSubMenuOpen] = useState(false);
-
-  function toggleSubMenu() {
-    setSubMenuOpen(!isSubMenuOpen);
-  }
-
+const MapWindow = ({ isOpen, setIsOpen, data }: ModalProps) => {
   return (
     <AnimatePresence>
       {isOpen && (
@@ -39,7 +15,6 @@ export default function MobileNav({
           exit={{ opacity: 0 }}
           onClick={() => {
             setIsOpen(false);
-            setSubMenuOpen(false);
           }}
           className="bg-slate-900/20 backdrop-blur  fixed h-screen inset-0 z-50 grid items-baseline overflow-y-scroll cursor-pointer"
         >
@@ -47,30 +22,23 @@ export default function MobileNav({
             initial={{ scale: 0, x: '100vw', y: '-100vh' }}
             animate={{ scale: 1, x: 0, y: 0 }}
             exit={{ scale: 0, x: '100vw', y: '-100vh' }}
-            transition={{ type: 'spring', stiffness: 100 }}
+            transition={{ type: 'spring', stiffness: 100, duration: 0.5 }}
             onClick={(e) => e.stopPropagation()}
-            className="bg-main-black border-2 border-solid border-main-yellow text-white p-6 rounded-lg w-full max-w-[328px] shadow-xl cursor-default relative overflow-hidden flex flex-col items-center gap-10 overflow-y-auto m-auto"
+            className="bg-main-black border-2 border-solid border-main-yellow text-white p-6 rounded-lg w-full max-w-[328px] lg:max-w-[640px] shadow-xl cursor-default relative overflow-hidden flex flex-col items-center gap-10 overflow-y-auto m-auto"
           >
             {/* Button Close */}
             <button
               onClick={() => setIsOpen(false)}
-              className={`absolute w-7 h-7 z-30 top-4 right-4 flex items-center justify-center hamburger ${
-                isOpen ? 'hamburger_active' : ''
-              }`}
-              aria-label="Close Mobile Menu"
+              className={`absolute w-7 h-7 z-30 top-4 right-4 flex items-center justify-center hamburger hamburger_active`}
             >
               <span />
             </button>
-            <div>
-              <iframe
-                src={`http://maps.google.com/maps?q=${location}&z=10&output=embed`}
-                width="100%"
-                height="450"
-              ></iframe>
-            </div>
+            <div>MODAL</div>
           </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
   );
-}
+};
+
+export default MapWindow;
