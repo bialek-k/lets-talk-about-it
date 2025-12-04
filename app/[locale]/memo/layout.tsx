513133fcc7eb43b3bdc0e2b6d74783e5feec7b4e
Vincent Słomiński <113301102+Okazaki92@users.noreply.github.com>
2025-12-05 00:15:20 +0100

    Update layout.tsx

--- app/[locale]/memo/layout.tsx
+++ app/[locale]/memo/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = {
   openGraph: {
     url: 'https://letstalkaboutit.pl/memo',
     title: "Gra Memo w Let's Talk About IT",
-    description: description: "Baw się z nami i zagraj w naszą grę memo w let's talkowym stylu",
+    description: "Baw się z nami i zagraj w naszą grę memo w let's talkowym stylu",
 ,
   },
 };
